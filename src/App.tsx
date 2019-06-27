e67373317defc855d04c9f6e7bd0f2e5c96245c1
Drew <drew.wiskus@space150.com>
2019-06-27 15:04:21 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Style } from "./types/Style";
+import "./config/styleReset.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// TODO
+// router
+// mobx
+// firebase
+
+export class App extends React.Component {
+  public render(): JSX.Element {
+    return <div style={styles.container} />;
+  }
 }
 
-export default App;
+const styles: Style = {
+  container: {
+    backgroundColor: "green",
+    height: `100vh`,
+    width: `100vw`
+  }
+};
